71e0a576599841c93a39d2ea6a0bb563d7e25ca1
root <root@arch.speedport.ip>
2016-04-01 23:50:18 +0200

    Project Zero

---
+++ app/src/main/java/com/paret/firstapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.paret.firstapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when the user touches the button
+     */
+    public void sendMessage(View view) {
+        Button b = (Button) view;
+        Context context = getApplicationContext();
+
+        int duration = Toast.LENGTH_SHORT;
+
+        String message;
+
+        Toast toast = Toast.makeText(context, getResources().getString(R.string.future_launch) + " " + b.getText().toString(), duration);
+        toast.show();
+    }
+}
